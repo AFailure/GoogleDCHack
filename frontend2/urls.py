7f80cc1261e504d1200f5cef247e9808086dabe7
SawcyD <eadeleke2004@gmail.com>
2024-02-23 00:51:20 -0500

    added the survey,

--- frontend2/urls.py
+++ frontend2/urls.py
@@ -16,8 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from .views import index
+from .views import index, places
 
 urlpatterns = [
     path('', index),
+    path('places/', places),
 ]
